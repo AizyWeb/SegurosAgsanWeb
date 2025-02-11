fe12db243775422030cb928d737ddc782ce6bca8
Andres Jimenez <jimenezcarballoane@gmail.com>
2025-02-11 12:08:36 +0100

    Fixed varColors not showing + added astro config {site}

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.segurosagsan.com",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss(),]
   }
 });
